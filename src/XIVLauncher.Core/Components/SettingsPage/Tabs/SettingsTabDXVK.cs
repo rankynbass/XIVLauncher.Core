6d0cd56b44b11d3cfa5cf8a4d40e902ab6ded260
Rankyn Bass <rankyn@proton.me>
2022-12-04 09:10:59 -0800

    Update frame rate message for 0 = unset

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDXVK.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDXVK.cs
@@ -15,7 +15,7 @@ public class SettingsTabDXVK : SettingsTab
             new SettingsEntry<Dxvk.DxvkVersion>("DXVK Version", "Choose which version of DXVK to use.", () => Program.Config.DxvkVersion, type => Program.Config.DxvkVersion = type),
             new SettingsEntry<bool>("Enable DXVK ASYNC", "Enable DXVK ASYNC patch.", () => Program.Config.DxvkAsyncEnabled ?? true, b => Program.Config.DxvkAsyncEnabled = b),
             new SettingsEntry<Dxvk.DxvkHudType>("DXVK Overlay", "DXVK Hud is included. MangoHud must be installed separately. ", () => Program.Config.DxvkHudType, type => Program.Config.DxvkHudType = type),
-            new NumericSettingsEntry("Frame Rate Limit", "Set a frame rate limit. DXVK will try not exceed it. Use 0 for no limit.", () => Program.Config.DxvkFrameRate ?? 0, i => Program.Config.DxvkFrameRate = i, 0, 1000),
+            new NumericSettingsEntry("Frame Rate Limit", "Set a frame rate limit, and DXVK will try not exceed it. Use 0 to leave unset.", () => Program.Config.DxvkFrameRate ?? 0, i => Program.Config.DxvkFrameRate = i, 0, 1000),
         };
     }
 
