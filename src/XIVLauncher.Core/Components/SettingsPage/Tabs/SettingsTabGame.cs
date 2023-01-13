3912d42cd3ef5cf48e3cdf84607a28a934ac98d3
Rankyn Bass <rankyn@proton.me>
2023-01-13 15:42:45 -0800

    Re-add Dx9/11 toggle

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
@@ -28,10 +28,15 @@ public class SettingsTabGame : SettingsTab
             CheckVisibility = () => Environment.OSVersion.Platform == PlatformID.Unix,
         },
 
+        new SettingsEntry<bool>("Use DirectX11", "", () => Program.Config.IsDx11 ?? true, x => Program.Config.IsDx11 = x)
+        {
+            CheckWarning = x => !x ? "DirectX 9 is no longer supported by the game or Dalamud. Things may not work." : null
+        },
+
         new SettingsEntry<string>("Additional Arguments", "Additional args to start the game with. NOT environtment variables.", () => Program.Config.AdditionalArgs, x => Program.Config.AdditionalArgs = x),
         new SettingsEntry<ClientLanguage>("Game Language", "", () => Program.Config.ClientLanguage ?? ClientLanguage.English, x => Program.Config.ClientLanguage = x),
         new SettingsEntry<DpiAwareness>("Game DPI Awareness", "Select the game's DPI Awareness. Change this if the game's scaling looks wrong.", () => Program.Config.DpiAwareness ?? DpiAwareness.Unaware, x => Program.Config.DpiAwareness = x),
-        new SettingsEntry<bool>("Free trial account", "Check this if you are using a free trial account.", () => Program.Config.IsFt ?? false, x => Program.Config.IsFt = x),
+        new SettingsEntry<bool>("Free trial account", "", () => Program.Config.IsFt ?? false, x => Program.Config.IsFt = x),
         new SettingsEntry<bool>("Ignore Steam", "Check this if you do not want XIVLauncher to communicate with Steam", () => Program.Config.IsIgnoringSteam ?? false, x => Program.Config.IsIgnoringSteam = x),
         new SettingsEntry<bool>("Use XIVLauncher authenticator/OTP macros", "", () => Program.Config.IsOtpServer ?? false, x => Program.Config.IsOtpServer = x),
         new NumericSettingsEntry("Launcher Font Size (Needs Restart)","", () => (int)(Program.Config.FontPxSize ?? 22), i => Program.Config.FontPxSize = (float)i, 12, 64),
