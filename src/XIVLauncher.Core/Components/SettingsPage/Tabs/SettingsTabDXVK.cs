a5a7aba7709cae32be198edf38ac65d4bd18b526
Rankyn Bass <rankyn@proton.me>
2023-01-24 20:39:10 -0800

    feat: Add dxvk 2.1 (no async)

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDXVK.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDXVK.cs
@@ -8,14 +8,18 @@ namespace XIVLauncher.Core.Components.SettingsPage.Tabs;
 
 public class SettingsTabDXVK : SettingsTab
 {
+    private SettingsEntry<Dxvk.DxvkVersion> dxvkVersion;
     private SettingsEntry<Dxvk.DxvkHudType> dxvkHudSetting;
 
     public SettingsTabDXVK()
     {
         Entries = new SettingsEntry[]
         {
-            new SettingsEntry<Dxvk.DxvkVersion>("DXVK Version", "Choose which version of DXVK to use.", () => Program.Config.DxvkVersion, type => Program.Config.DxvkVersion = type),
-            new SettingsEntry<bool>("Enable DXVK ASYNC", "Enable DXVK ASYNC patch.", () => Program.Config.DxvkAsyncEnabled ?? true, b => Program.Config.DxvkAsyncEnabled = b),
+            dxvkVersion = new SettingsEntry<Dxvk.DxvkVersion>("DXVK Version", "Choose which version of DXVK to use.", () => Program.Config.DxvkVersion, type => Program.Config.DxvkVersion = type),
+            new SettingsEntry<bool>("Enable DXVK ASYNC", "Enable DXVK ASYNC patch.", () => Program.Config.DxvkAsyncEnabled ?? true, b => Program.Config.DxvkAsyncEnabled = b)
+            {
+                CheckVisibility = () => dxvkVersion.Value != Dxvk.DxvkVersion.v2_1,
+            },
             dxvkHudSetting = new SettingsEntry<Dxvk.DxvkHudType>("DXVK Overlay", "DXVK Hud is included. MangoHud must be installed separately.\nFlatpak XIVLauncher needs flatpak MangoHud.", () => Program.Config.DxvkHudType, type => Program.Config.DxvkHudType = type)
             {
                 CheckValidity = type =>
