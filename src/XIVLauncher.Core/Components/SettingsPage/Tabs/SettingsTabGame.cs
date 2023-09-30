d96e92d407c89f3c9a08282b0756c4c44df2d26a
Rankyn Bass <rankyn@proton.me>
2023-09-30 12:49:57 -0700

    Add buttons to reset Game/Config paths

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabGame.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using XIVLauncher.Common;
 using ImGuiNET;
 
@@ -5,48 +6,89 @@ namespace XIVLauncher.Core.Components.SettingsPage.Tabs;
 
 public class SettingsTabGame : SettingsTab
 {
-    public override SettingsEntry[] Entries { get; } =
+
+    private SettingsEntry<DirectoryInfo> gamePath;
+
+    private SettingsEntry<DirectoryInfo> gameConfigPath;
+
+    public SettingsTabGame()
     {
-        new SettingsEntry<DirectoryInfo>("Game Path", "Where the game is or will be installed.", () => Program.Config.GamePath, x => Program.Config.GamePath = x)
+        Entries = new SettingsEntry[]
         {
-            CheckValidity = x =>
+            gamePath = new SettingsEntry<DirectoryInfo>("Game Path", "Where the game is or will be installed.", () => Program.Config.GamePath, x => Program.Config.GamePath = x)
             {
-                if (string.IsNullOrWhiteSpace(x?.FullName))
-                    return "Game path is not set.";
+                CheckValidity = x =>
+                {
+                    if (string.IsNullOrWhiteSpace(x?.FullName))
+                        return "Game path is not set.";
 
-                if (x.Name == "game" || x.Name == "boot")
-                    return "Please select the path containing the folders \"game\" and \"boot\", not the folders itself.";
+                    if (x.Name == "game" || x.Name == "boot")
+                        return "Please select the path containing the folders \"game\" and \"boot\", not the folders itself.";
 
-                return null;
-            }
-        },
+                    return null;
+                }
+            },
 
-        new SettingsEntry<DirectoryInfo>("Game Configuration Path", "Where your user config files will be stored.", () => Program.Config.GameConfigPath, x => Program.Config.GameConfigPath = x)
-        {
-            CheckValidity = x => string.IsNullOrWhiteSpace(x?.FullName) ? "Game Config Path is not set." : null,
+            gameConfigPath = new SettingsEntry<DirectoryInfo>("Game Configuration Path", "Where your user config files will be stored.", () => Program.Config.GameConfigPath, x => Program.Config.GameConfigPath = x)
+            {
+                CheckValidity = x => string.IsNullOrWhiteSpace(x?.FullName) ? "Game Config Path is not set." : null,
 
-            // TODO: We should also support this on Windows
-            CheckVisibility = () => Environment.OSVersion.Platform == PlatformID.Unix,
-        },
+                // TODO: We should also support this on Windows
+                CheckVisibility = () => Environment.OSVersion.Platform == PlatformID.Unix,
+            },
 
-        new SettingsEntry<bool>("Use DirectX11", "Use the modern DirectX11 version of the game.", () => Program.Config.IsDx11 ?? true, x => Program.Config.IsDx11 = x)
-        {
-            CheckWarning = x => !x ? "DirectX 9 is no longer supported by the game or Dalamud. Things may not work." : null
-        },
+            new SettingsEntry<bool>("Use DirectX11", "Use the modern DirectX11 version of the game.", () => Program.Config.IsDx11 ?? true, x => Program.Config.IsDx11 = x)
+            {
+                CheckWarning = x => !x ? "DirectX 9 is no longer supported by the game or Dalamud. Things may not work." : null
+            },
 
-        new SettingsEntry<string>("Additional Game Arguments", "Follows Steam conventions: VAR1=value VAR2=value %command% -arg1 -arg2.\nCan't pass programs (like gamescope -- %command%). Does not accept flatpak args (--parent-pid=1, etc.)", () => Program.Config.AdditionalArgs, x => Program.Config.AdditionalArgs = x),
-        new SettingsEntry<ClientLanguage>("Game Language", "Select the game's language.", () => Program.Config.ClientLanguage ?? ClientLanguage.English, x => Program.Config.ClientLanguage = x),
-        new SettingsEntry<DpiAwareness>("Game DPI Awareness", "Select the game's DPI Awareness. Change this if the game's scaling looks wrong.", () => Program.Config.DpiAwareness ?? DpiAwareness.Unaware, x => Program.Config.DpiAwareness = x),
-        new SettingsEntry<bool>("Free Trial Account", "Check this if you are using a free trial account.", () => Program.Config.IsFt ?? false, x => Program.Config.IsFt = x),
-        new SettingsEntry<bool>("Use XIVLauncher authenticator/OTP macros", "Check this if you want to use the XIVLauncher authenticator app or macros.", () => Program.Config.IsOtpServer ?? false, x => Program.Config.IsOtpServer = x),
-        // new SettingsEntry<bool>("Ignore Steam", "Check this if you do not want XIVLauncher to communicate with Steam (Requires Restart).", () => Program.Config.IsIgnoringSteam ?? false, x => Program.Config.IsIgnoringSteam = x),
-        new SettingsEntry<bool>("Use Experimental UID Cache", "Tries to save your login token for the next start. Can result in launching with expired sessions.\nDisable if receiving FFXIV error 1012 or 500X.", () => Program.Config.IsUidCacheEnabled ?? false, x => Program.Config.IsUidCacheEnabled = x),
-    };
+            new SettingsEntry<string>("Additional Game Arguments", "Follows Steam conventions: VAR1=value VAR2=value %command% -arg1 -arg2.\nCan't pass programs (like gamescope -- %command%). Does not accept flatpak args (--parent-pid=1, etc.)", () => Program.Config.AdditionalArgs, x => Program.Config.AdditionalArgs = x),
+            new SettingsEntry<ClientLanguage>("Game Language", "Select the game's language.", () => Program.Config.ClientLanguage ?? ClientLanguage.English, x => Program.Config.ClientLanguage = x),
+            new SettingsEntry<DpiAwareness>("Game DPI Awareness", "Select the game's DPI Awareness. Change this if the game's scaling looks wrong.", () => Program.Config.DpiAwareness ?? DpiAwareness.Unaware, x => Program.Config.DpiAwareness = x),
+            new SettingsEntry<bool>("Free Trial Account", "Check this if you are using a free trial account.", () => Program.Config.IsFt ?? false, x => Program.Config.IsFt = x),
+            new SettingsEntry<bool>("Use XIVLauncher authenticator/OTP macros", "Check this if you want to use the XIVLauncher authenticator app or macros.", () => Program.Config.IsOtpServer ?? false, x => Program.Config.IsOtpServer = x),
+            // new SettingsEntry<bool>("Ignore Steam", "Check this if you do not want XIVLauncher to communicate with Steam (Requires Restart).", () => Program.Config.IsIgnoringSteam ?? false, x => Program.Config.IsIgnoringSteam = x),
+            new SettingsEntry<bool>("Use Experimental UID Cache", "Tries to save your login token for the next start. Can result in launching with expired sessions.\nDisable if receiving FFXIV error 1012 or 500X.", () => Program.Config.IsUidCacheEnabled ?? false, x => Program.Config.IsUidCacheEnabled = x),
+        };
+    }
+
+    public override SettingsEntry[] Entries { get; }
 
     public override string Title => "Game";
 
     public override void Draw()
     {
         base.Draw();
+
+        var gamePathIsSame = Program.SteamCompatPath == gamePath.Value.FullName;
+        var gameConfigIsSame = Program.LocalStorage == gameConfigPath.Value.FullName;
+
+        if (gamePathIsSame && gameConfigIsSame) return;
+
+        ImGui.Separator();
+        
+        ImGui.Dummy(new Vector2(10) * ImGuiHelpers.GlobalScale);
+
+        if (!gamePathIsSame)
+        {
+            if (ImGui.Button("Reset Game Path"))
+            {
+                Program.Config.GamePath = new DirectoryInfo(Program.SteamCompatPath);
+                gamePath.Load();
+                this.Save();
+            }
+
+            ImGui.SameLine();
+        }
+
+        if (!gameConfigIsSame)
+        {
+            if (ImGui.Button("Reset Game Config Path"))
+            {
+                Program.Config.GameConfigPath = new DirectoryInfo(Program.LocalStorage);
+                gameConfigPath.Load();
+                this.Save();
+            }
+        }
     }
 }
