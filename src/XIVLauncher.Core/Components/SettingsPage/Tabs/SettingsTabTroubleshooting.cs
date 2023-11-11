5ba96602876c1f6a76e1eccc73cc7f37ef23c347
Rankyn Bass <rankyn@proton.me>
2023-11-10 21:15:41 -0800

    Make Clear Plugins separate from Clear Dalamud

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabTroubleshooting.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabTroubleshooting.cs
@@ -37,10 +37,16 @@ public class SettingsTabTroubleshooting : SettingsTab
             Program.ClearTools(true);
         }
 
-        ImGui.Text("\nClear all the files and folders related to Dalamud. Your settings will not be touched,\nbut all your plugins will be uninstalled, including custom repos.");
+        ImGui.Text("\nClear all the files and folders related to Dalamud. This will not uninstall your plugins or their configurations.");
         if (ImGui.Button("Clear Dalamud"))
         {
-            Program.ClearPlugins(true);
+            Program.ClearDalamud(true);
+        }
+
+        ImGui.Text("\nClear the installedPlugins folder. This will uninstall your plugins, but will not remove their configurations.");
+        if (ImGui.Button("Clear Plugins"))
+        {
+            Program.ClearPlugins();
         }
 
         ImGui.Text("\nClear all the log files.");
--- src/XIVLauncher.Core/Program.cs
+++ src/XIVLauncher.Core/Program.cs
@@ -173,6 +173,7 @@ class Program
         {
             if (CoreEnvironmentSettings.ClearSettings) ClearSettings();
             if (CoreEnvironmentSettings.ClearPrefix) ClearPrefix();
+            if (CoreEnvironmentSettings.ClearDalamud) ClearDalamud();
             if (CoreEnvironmentSettings.ClearPlugins) ClearPlugins();
             if (CoreEnvironmentSettings.ClearTools) ClearTools();
             if (CoreEnvironmentSettings.ClearLogs) ClearLogs();
@@ -413,17 +414,14 @@ class Program
         storage.GetFolder("protonprefix/pfx");
     }
 
-    public static void ClearPlugins(bool tsbutton = false)
+    public static void ClearDalamud(bool tsbutton = false)
     {
         storage.GetFolder("dalamud").Delete(true);
         storage.GetFolder("dalamudAssets").Delete(true);
-        storage.GetFolder("installedPlugins").Delete(true);
         storage.GetFolder("runtime").Delete(true);
         if (storage.GetFile("dalamudUI.ini").Exists) storage.GetFile("dalamudUI.ini").Delete();
-        if (storage.GetFile("dalamudConfig.json").Exists) storage.GetFile("dalamudConfig.json").Delete();
         storage.GetFolder("dalamud");
         storage.GetFolder("dalamudAssets");
-        storage.GetFolder("installedPlugins");
         storage.GetFolder("runtime");
         if (tsbutton)
         {
@@ -434,6 +432,13 @@ class Program
             };
             DalamudUpdater.Run();
         }
+    }
+
+    public static void ClearPlugins()
+    {
+        storage.GetFolder("installedPlugins").Delete(true);
+        storage.GetFolder("installedPlugins");
+        if (storage.GetFile("dalamudConfig.json").Exists) storage.GetFile("dalamudConfig.json").Delete();
     }
 
     public static void ClearTools(bool tsbutton = false)
@@ -473,7 +478,8 @@ class Program
     {
         ClearSettings(tsbutton);
         ClearPrefix();
-        ClearPlugins(tsbutton);
+        ClearDalamud(tsbutton);
+        ClearPlugins();
         ClearTools(tsbutton);
         ClearLogs(true);
     }
