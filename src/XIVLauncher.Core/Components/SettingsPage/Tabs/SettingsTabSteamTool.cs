d6aed527b74c9408df022cce6a5de676e24a5021
Rankyn Bass <rankyn@proton.me>
2023-11-26 14:06:31 -0800

    Update steam tool instructions

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabSteamTool.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabSteamTool.cs
@@ -24,9 +24,11 @@ public class SettingsTabSteamTool : SettingsTab
         {
             steamPath = new SettingsEntry<string>("Steam Path (native install)", "Path to the native steam config files. Only change this if you have your steam config stored somewhere else.",
                 () => Program.Config.SteamPath ?? Path.Combine(CoreEnvironmentSettings.XDG_DATA_HOME, "Steam"), s => Program.Config.SteamPath = s),
-
             steamFlatpakPath = new SettingsEntry<string>("Steam Path (flatpak install)", "Path to the flatpak steam config files. Only change this if you have your steam config stored somewhere else.",
-                () => Program.Config.SteamFlatpakPath ?? Path.Combine(CoreEnvironmentSettings.HOME, ".var", "app", "com.valvesoftware.Steam", "data", "Steam" ), s => Program.Config.SteamFlatpakPath = s),           
+                () => Program.Config.SteamFlatpakPath ?? Path.Combine(CoreEnvironmentSettings.HOME, ".var", "app", "com.valvesoftware.Steam", "data", "Steam" ), s => Program.Config.SteamFlatpakPath = s)
+                {
+                    CheckVisibility = () => OSInfo.IsFlatpak,
+                },           
         };
     }
 
@@ -52,11 +54,17 @@ public class SettingsTabSteamTool : SettingsTab
         ImGui.Text("\nUse this tab to install XIVLauncher.Core as a Steam compatibility tool.");
         ImGui.Text("\nAfter you have installed XIVLauncher as a Steam tool, close this program, and launch Steam. Select Final Fantasy XIV from the library,");
         ImGui.Text("and go to Compatibility. Force the use of a specific Steam Play compatibility tool, and choose XIVLauncher.Core as Compatibility Tool.");
-        ImGui.Text("XIVLauncher.Core will now be used to launch Final Fantasy XIV. This feature can be used with Flatpak steam.");
-        ImGui.Text("\nIf you wish to install into Flatpak Steam, you must use Flatseal to give XIVLauncher access to Steam's flatpak path. This is probably something like:");
-        ImGui.Text($"{CoreEnvironmentSettings.HOME}/.var/app/com.valvesoftware.Steam. If you do not give this permission, installation will fail. You will probably also want to");
-        ImGui.Text($"give Steam permission to {CoreEnvironmentSettings.HOME}/.xlcore, so that you can continue to use your current xlcore folder.");
-
+        ImGui.Text("XIVLauncher.Core will now be used to launch Final Fantasy XIV.");
+        if (OSInfo.IsFlatpak)
+        {
+            ImGui.Text("\nIf you wish to install into flatpak Steam, you must use Flatseal to give XIVLauncher access to Steam's flatpak path. This is probably something like:");
+            ImGui.Text($"{CoreEnvironmentSettings.HOME}/.var/app/com.valvesoftware.Steam. If you do not give this permission, installation will fail. You will probably also want to");
+            ImGui.Text($"give Steam permission to {CoreEnvironmentSettings.HOME}/.xlcore, so that you can continue to use your current xlcore folder.");
+        }
+        else
+        {
+            ImGui.Text("\nIf you wish to install XIVLauncher to flatpak Steam, please run the flatpak version of XIVLauncher.");
+        }
         ImGui.Dummy(new Vector2(10));        
         ImGui.Separator();
         ImGui.Dummy(new Vector2(10));
