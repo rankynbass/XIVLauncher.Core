300c70276874c4e87085b2dddf07fa4723d43ffa
Rankyn Bass <rankyn@proton.me>
2024-09-02 16:23:45 -0700

    Update Clear Wine & DXVK text

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabTroubleshooting.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabTroubleshooting.cs
@@ -38,7 +38,7 @@ public class SettingsTabTroubleshooting : SettingsTab
             Program.ClearPrefix();
         }
 
-        ImGui.Text("\nClear the managed Wine and DXVK installs. Custom versions won't be touched.");
+        ImGui.Text("\nClear all Wine, DXVK, and DXVK-Nvapi installs.");
         if (ImGui.Button("Clear Wine & DXVK"))
         {
             Program.ClearTools(true);
