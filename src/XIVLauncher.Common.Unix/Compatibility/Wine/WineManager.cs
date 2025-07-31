014a3790e3402359a2ee9fccf88bb8ea0d611fbb
Rankyn Bass <rankyn@proton.me>
2025-07-30 20:09:13 -0700

    Add loading Wine from RB-WineVersions.json

--- src/XIVLauncher.Common.Unix/Compatibility/Wine/WineManager.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/Wine/WineManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Linq;
+using Newtonsoft.Json;
+using Serilog;
 
 using XIVLauncher.Common.Unix.Compatibility.Wine.Releases;
 
@@ -81,7 +83,11 @@ public class WineManager
         // Umu Launcher
         this.umuFolder = Path.Combine(root, "compatibilitytool", "umu");
 
-        Initialize();
+        var wineJson = new FileInfo(Path.Combine(rootFolder, "RB-WineVersions.json"));
+        if (wineJson.Exists)
+            InitializeJson(wineJson);
+        else
+            Initialize();
     }
 
     public void SetUmuLauncher(bool useBuiltinUmu)
@@ -129,6 +135,38 @@ public class WineManager
         AddVersion(protonStable);
         AddVersion(protonStableNtsync);
         AddVersion(protonLegacy);
+    }
+
+    private void InitializeJson(FileInfo wineJson)
+    {
+        Version = new Dictionary<string, IWineRelease>();
+        Console.WriteLine("RB-WineVersions.json fould!");
+        var fileStream = wineJson.OpenRead();
+        List<DeserializedWine> wineList;
+        using (StreamReader file = new StreamReader(fileStream))
+        {
+            wineList = JsonConvert.DeserializeObject<List<DeserializedWine>>(file.ReadToEnd());
+        }
+        Console.WriteLine("Loading Wine Versions from file:");
+        foreach (var wineRelease in wineList)
+        {
+            if (wineRelease.IsProton)
+            {
+                AddVersion(new ProtonCustomRelease(wineRelease.Label, wineRelease.Description, wineRelease.Name, this.compatFolder, wineRelease.DownloadUrl, wineRelease.Checksums[0]));
+            }
+            else
+            {
+                AddVersion(new WineCustomRelease(wineRelease.Label, wineRelease.Description, wineRelease.Name, this.wineFolder, wineRelease.DownloadUrl, wineRelease.Lsteamclient, wineRelease.Checksums));
+            }
+            Console.WriteLine($"{wineRelease.Label} - {wineRelease.Description} ({(wineRelease.IsProton ? "Proton" : "Wine")})");
+        }
+        this.LEGACY = "wine-xiv-staging-fsync-git-8.5.r4.g4211bac7";
+        this.DEFAULT = Version.FirstOrDefault().Key;
+
+        foreach (var name in Version)
+        {
+            Console.WriteLine(name.Key + " - IsProton: " + name.Value.IsProton.ToString());
+        }
     }
 
     private void AddVersion(IWineRelease wine)
