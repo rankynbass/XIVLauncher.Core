a8f565f41e616391be9d45f3f5e4f99194cddfae
Rankyn Bass <rankyn@proton.me>
2025-12-24 14:49:36 -0800

    Add beta installer scripts

---
+++ xlm/install-beta-steamdeck.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "-- XLM Steamdeck RB-Beta Auto-Installer --"
+echo ""
+
+echo "[Step: 1] Downloading XLM"
+curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64-unknown-linux-gnu > /tmp/xlm
+
+echo "[Step: 2] Configuring XLM as a Steam Tool using XIVLauncher-RB"
+chmod +x /tmp/xlm
+mkdir -p /tmp/xlm-rb
+/tmp/xlm install-steam-tool --extra-launch-args="--use-fallback-secret-provider --xlcore-web-release-url-base=\"https://github.com/rankynbass/XIVLauncher.Core/releases/download/rb-v1.3.1.1-beta1/\"" --steam-compat-path /tmp/xlm-rb
+sed -i 's/XLCore/XLCore-RB-beta/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
+sed -i 's/"xlm"/"xlm-rb-beta"/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
+sed -i 's/"xlm"/"xlm-rb-beta"/' /tmp/xlm-rb/XLM/toolmanifest.vdf
+mkdir -p ~/.steam/root/compatibilitytools.d/XLM-RB-beta
+mv /tmp/xlm-rb/XLM/* ~/.steam/root/compatibilitytools.d/XLM-RB-beta/
+
+echo "[Step: 3] Cleanup XLM binary"
+rm /tmp/xlm
+
+echo ""
+echo "-- Auto Installer Complete: Restart Steam and follow the README to continue! --"
