d1d7f8faf399c551568661e01d22a685a3222c12
Rankyn Bass <rankyn@proton.me>
2026-01-11 19:42:21 -0800

    Fix beta scripts (add / to url)

--- xlm/install-beta-steamdeck.sh
+++ xlm/install-beta-steamdeck.sh
@@ -9,7 +9,7 @@ curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64-unknow
 echo "[Step: 2] Configuring XLM as a Steam Tool using XIVLauncher-RB"
 chmod +x /tmp/xlm
 mkdir -p /tmp/xlm-rb
-/tmp/xlm install-steam-tool --extra-launch-args="--use-fallback-secret-provider --xlcore-web-release-url-base=\"https://github.com/rankynbass/XIVLauncher.Core/releases/download/rb-v1.3.1.1-beta2\"" --steam-compat-path /tmp/xlm-rb
+/tmp/xlm install-steam-tool --extra-launch-args="--use-fallback-secret-provider --xlcore-web-release-url-base=\"https://github.com/rankynbass/XIVLauncher.Core/releases/download/rb-v1.3.1.1-beta2/\"" --steam-compat-path /tmp/xlm-rb
 sed -i 's/XLCore/XLCore-RB-beta/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
 sed -i 's/"xlm"/"xlm-rb-beta"/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
 sed -i 's/"xlm"/"xlm-rb-beta"/' /tmp/xlm-rb/XLM/toolmanifest.vdf
