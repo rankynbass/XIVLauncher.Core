4c4a4d0a5b10a415119bd1fc8517d520be4678b9
Rankyn Bass <rankyn@proton.me>
2026-04-09 18:26:45 -0700

    Finish merging changes

--- src/XIVLauncher.Common.Unix/Compatibility/Wine/Releases/WineStable.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/Wine/Releases/WineStable.cs
@@ -2,11 +2,11 @@ namespace XIVLauncher.Common.Unix.Compatibility.Wine.Releases;
 
 public sealed class WineStableRelease(WineReleaseDistro wineDistroId, string parentFolder) : IWineRelease
 {
-    public string Name { get; } = $"wine-xiv-staging-fsync-git-10.8.r0.g47f77594-nolsc";
+    public string Name { get; } = $"wine-xiv-staging-fsync-git-10.8.r0.a2ca9e4-nolsc";
     public string ParentFolder { get; } = parentFolder;
     public string Label { get; } = "Stable";
     public string Description { get; } = "Based on Wine 10.8 - recommended for most users.";
-    public string DownloadUrl { get; } = $"https://github.com/goatcorp/wine-xiv-git/releases/download/10.8.r0.g47f77594/wine-xiv-staging-fsync-git-{wineDistroId}-10.8.r0.g47f77594-nolsc.tar.xz";
+    public string DownloadUrl { get; } = $"https://github.com/goatcorp/wine-xiv-git/releases/download/10.8.r0.a2ca9e4/wine-xiv-staging-fsync-git-{wineDistroId}-10.8.r0.a2ca9e4-nolsc.tar.xz";
     public string[] Checksums { get; } = [
         "a4ff42d8e7a8057f794b3f3f6a686b21411f091c8264acd419869c726a1d21b594b2e024b7b995fea4fb99f3b4538fdda78eb0ab7a44d0905bc1272102eecf6e", // wine-xiv-staging-fsync-git-arch-10.8.r0.a2ca9e4-nolsc.tar.xz
         "1693253de16618db15ee51e1a96af02330eccccba4dc3f0710a63c920e33d5063c67c6b7d4950b61be61105f14eb301ef861278a057e6b4d31ccf8c58f126e60",  // wine-xiv-staging-fsync-git-fedora-10.8.r0.a2ca9e4-nolsc.tar.xz
